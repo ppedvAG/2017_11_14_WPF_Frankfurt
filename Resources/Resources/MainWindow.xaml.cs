0e10078be96762fbd9af92c1ddab2b6f8edce347
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-14 16:19:24 +0100

    Resources

---
+++ Resources/Resources/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace Resources
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void ChangeColor(object sender, RoutedEventArgs e)
+        {
+            if (FindResource("defaultBackgroundColor") is SolidColorBrush brush)
+                brush.Color = Colors.DarkOliveGreen;
+        }
+
+        private void ChangeInstance(object sender, RoutedEventArgs e) => Resources["defaultBackgroundColor"] = new SolidColorBrush(Colors.Indigo);
+
+        private void ChangeType(object sender, RoutedEventArgs e) => Resources["defaultBackgroundColor"] = new RadialGradientBrush(
+            Colors.YellowGreen,
+            Colors.Blue);
+    }
+}
