e26ae60f153ad38e0ea4fa682c34a28eba0848d1
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-15 14:22:37 +0100

    Source

---
+++ Binding/Binding/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Binding
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+
+    internal class Customer : Person
+    {
+        public int CountOrders { get; set; }
+    }
+
+    internal class Employee : Person
+    {
+        public int Salary { get; set; }
+    }
+}
