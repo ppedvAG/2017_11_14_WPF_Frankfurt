1959af5f56ee8d31d170b1b0b8f72d47a4801814
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-15 11:24:20 +0100

    Basic Loaclization

---
+++ Localization/Localization/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Threading;
+using System.Windows;
+
+namespace Localization
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-GB");
+        }
+    }
+}
