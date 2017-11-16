7bc4c650625d7d6cae8889484208ed0feca96656
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-16 14:47:31 +0100

    Validation

---
+++ Validation/Validation/MustNotBe16ValidationRule.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace Validation
+{
+    internal class MustNotBe16ValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo) =>
+            (string)value == "16"
+                ? new ValidationResult(false, "Sechzehn ist ein ungültiger Wert! Bitte wenden Sie sich an ihren Systemadministrator.")
+                : ValidationResult.ValidResult;
+    }
+}
