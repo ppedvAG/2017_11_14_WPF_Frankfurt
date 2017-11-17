00c6835944bec913b725e26e2c9ea79511f21e6b
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-17 12:34:26 +0100

    Routed Events

---
+++ RoutedEventsExample/RoutedEventsExample/LogItem.cs
@@ -0,0 +1,9 @@
+﻿
+namespace RoutedEventsExample
+{
+    public class LogItem
+    {
+        public string EventName { get; set; }
+        public string Sender { get; set; }
+    }
+}
---
+++ RoutedEventsExample/RoutedEventsExample/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace RoutedEventsExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ObservableCollection<LogItem> logItems;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            logItems = new ObservableCollection<LogItem>();
+            EventsDataGrid.ItemsSource = logItems;
+        }
+
+        private void PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var element = sender as FrameworkElement;
+
+            if(element == BlueRectangle)
+            {
+                e.Handled = true;
+            }
+
+            logItems.Add(new LogItem { EventName = "PreviewMouseDown", Sender = element?.Name });
+        }
+
+        private void MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var element = sender as FrameworkElement;
+            logItems.Add(new LogItem { EventName = "MouseDown", Sender = element?.Name });
+        }
+
+        private void MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            var element = sender as FrameworkElement;
+            logItems.Add(new LogItem { EventName = "MouseUp", Sender = element?.Name });
+        }
+
+        private void PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            var element = sender as FrameworkElement;
+            logItems.Add(new LogItem { EventName = "PreviewMouseUp", Sender = element?.Name });
+        }
+    }
+}
