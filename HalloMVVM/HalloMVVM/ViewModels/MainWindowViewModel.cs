0748dbc0657d66ff23e8e11d991f6e33cd8dc3f3
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-17 10:43:28 +0100

    ViewModelBase and INotifyPropertyChanged

---
+++ HalloMVVM/HalloMVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HalloMVVM.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        private string welcomeText = "Hallo MVVM.";
+        public string WelcomeText
+        {
+            get => welcomeText;
+            set => Set(ref welcomeText, value);
+        }
+    }
+}
---
+++ HalloMVVM/HalloMVVM/ViewModels/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HalloMVVM.ViewModels
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        protected void Set<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if(field == null || !field.Equals(newValue))
+            {
+                field = newValue;
+                RaisePropertyChanged(propertyName);
+            }
+        }
+    }
+}
