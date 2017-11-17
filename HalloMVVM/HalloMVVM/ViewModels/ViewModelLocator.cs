953e066779932d2d2af0ac272fd622164f4c65be
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-11-17 12:09:22 +0100

    ViewModelLocator

---
+++ HalloMVVM/HalloMVVM/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,12 @@
+﻿namespace HalloMVVM.ViewModels
+{
+    internal class ViewModelLocator
+    {
+        public MainWindowViewModel MainWindowViewModel { get; }
+
+        public ViewModelLocator()
+        {
+            MainWindowViewModel = new MainWindowViewModel(/* new DataService() */);
+        }
+    }
+}
